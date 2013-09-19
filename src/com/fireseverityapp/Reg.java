47c10aa0274b2ac92c482f82dfc810dc13037ab3
pnkjkshp80 <pnkjkshp80@hmail.com>
2013-09-19 19:18:19 +0930

    Pankaj----action commit

---
+++ src/com/fireseverityapp/Reg.java
@@ -0,0 +1,81 @@
+package com.fireseverityapp;
+
+public class Reg {
+
+	// private variables
+	int _id;
+	String _name;
+	String _email;
+	String _org;
+	String _desig;
+
+	public String get_org() {
+		return _org;
+	}
+
+	public void set_org(String _org) {
+		this._org = _org;
+	}
+
+	// Empty constructor
+	public Reg(String string, String string2, String string3, String string4) {
+		this._name = string;
+		this._org = string2;
+		this._desig = string3;
+		this._email = string4;
+
+	}
+
+	// Empty constructor
+	public Reg() {
+
+	}
+
+	// constructor
+	public Reg(int id, String name) {
+		this._id = id;
+		this._name = name;
+	}
+
+	// constructor
+	public Reg(String name) {
+		this._name = name;
+	}
+
+	// getting ID
+	public int getID() {
+		return this._id;
+	}
+
+	// setting id
+	public void setID(int id) {
+		this._id = id;
+	}
+
+	// getting name
+	public String getName() {
+		return this._name;
+	}
+
+	// setting name
+	public void setName(String name) {
+		this._name = name;
+	}
+
+	public String get_email() {
+		return _email;
+	}
+
+	public void set_email(String _email) {
+		this._email = _email;
+	}
+
+	public String get_desig() {
+		return _desig;
+	}
+
+	public void set_desig(String _desig) {
+		this._desig = _desig;
+	}
+
+}
---
+++ src/com/fireseverityapp/RegistrationActivity.java
@@ -0,0 +1,102 @@
+package com.fireseverityapp;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegistrationActivity extends Activity {
+	EditText name, organisation, designation, email;
+	String reg_name, reg_organisation, reg_designation, reg_email;
+	Button submit;
+	DatabaseHandler db;
+	private final int PLEASE_WAIT = 1;
+
+	private Handler mHandler;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_registration);
+		name = (EditText) findViewById(R.id.tv_name);
+		organisation = (EditText) findViewById(R.id.tv_organisation);
+		designation = (EditText) findViewById(R.id.tv_Designation);
+		email = (EditText) findViewById(R.id.tv_Email);
+		submit = (Button) findViewById(R.id.btn_Submit);
+		db = new DatabaseHandler(this);
+
+		// reg_name=name.getText().toString();
+
+		submit.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				addUser();
+				Intent i=new Intent(RegistrationActivity.this,wellcome.class);
+				startActivity(i);
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_registration, menu);
+		return true;
+	}
+
+	
+	
+	public void addUser() {
+		
+		Log.d("Insert: ", "Inserting ..");
+		db.addContact(new Reg(name.getText().toString(),organisation.getText().toString(),designation.getText().toString(),email.getText().toString()));
+		
+		
+		Log.d("Reading: ", "Reading all contacts..");
+		List<Reg> contacts = db.getAllContacts();
+
+		for (Reg cn : contacts) {
+			String log = "Id: " + cn.getID() + " ,Name: " + cn.getName()+ " ,email: " + cn.get_email()+ " ,org: " + cn.get_org()+ " ,desig: " + cn.get_desig();
+		
+			Log.d("Name: ", log);
+							
+
+		}
+		
+	
+	}
+	
+	
+	
+	/*private void inFo(String message) {
+		String help = message;
+		AlertDialog.Builder builder = new AlertDialog.Builder(RegistrationActivity.this);
+		builder.setMessage(help).setCancelable(true);
+		builder.setTitle("xyz");
+		builder.setIcon(R.drawable.ic_launcher);
+		builder.setCancelable(true);
+		builder.setIcon(null);
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+			public void onClick(DialogInterface dialog, int id) {
+				dialog.cancel();
+			}
+
+		});
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+	*/
+}
